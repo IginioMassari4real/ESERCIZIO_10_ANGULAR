80b6799720d414e384500a459800586a099328be
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-04-18 12:38:59 +0000

    progetto creato

---
+++ Esercizio_10/src/app/first-component/first-component.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.css'
+})
+export class FirstComponentComponent {
+
+}
