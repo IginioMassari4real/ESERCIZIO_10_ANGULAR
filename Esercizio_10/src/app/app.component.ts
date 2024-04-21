4b873636c4315da1a6674abf0536cc56c8428d0b
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-04-21 09:13:41 +0000

    progetto iniziato

--- Esercizio_10/src/app/app.component.ts
+++ Esercizio_10/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ListComponentComponent } from './list/list-component.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MostreComponentComponent } from './mostre/mostre-component.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ListComponentComponent, NavigationComponent, MostreComponentComponent, IntroductionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Esercizio_10';
+
+  bottone:boolean=true
+
+  onRiceviDati(value: any){
+
+    this.bottone=value
+}
 }
---
+++ Esercizio_10/src/app/list/list-component.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-component.component.html',
+  styleUrl: './list-component.component.css'
+})
+export class ListComponentComponent {
+
+  mostre = [
+
+   {nome:"", artisti:"", descrizione:"", datainizio: new Date(2024, 5, 1), datafine: new Date(2024, 5, 1), immagine:"",},
+
+   {nome:"", artisti:"", descrizione:"", datainizio: new Date(2024, 5, 1), datafine: new Date(2024, 5, 1), immagine:"",},
+
+   {nome:"", artisti:"", descrizione:"", datainizio: new Date(2024, 5, 1), datafine: new Date(2024, 5, 1), immagine:"",},
+
+   {nome:"", artisti:"", descrizione:"", datainizio: new Date(2024, 5, 1), datafine: new Date(2024, 5, 1), immagine:"",},
+
+   {nome:"", artisti:"", descrizione:"", datainizio: new Date(2024, 5, 1), datafine: new Date(2024, 5, 1), immagine:"",},
+
+   {nome:"", artisti:"", descrizione:"", datainizio: new Date(2024, 5, 1), datafine: new Date(2024, 5, 1), immagine:"",},
+
+   {nome:"", artisti:"", descrizione:"", datainizio: new Date(2024, 5, 1), datafine: new Date(2024, 5, 1), immagine:"",},
+
+  ]
+}
