30f11e19ad7d5b368372260b15740aaef58108a3
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-04-22 21:09:42 +0000

    progetto quasi finito

--- Esercizio_10/src/app/app.routes.ts
+++ Esercizio_10/src/app/app.routes.ts
@@ -3,4 +3,14 @@ import { ListComponentComponent } from './list/list-component.component';
 import { MostreComponentComponent } from './mostre/mostre-component.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 
-export const routes: Routes = [{path: 'listaMostre', component: ListComponentComponent}, {path: 'Mostre/:id', component: MostreComponentComponent} ,{ path: 'Home', component:IntroductionComponent} ];
+export const routes: Routes = [
+
+    {path: '', redirectTo: '/Home', pathMatch: 'full'},
+    
+    {path: 'listaMostre', component: ListComponentComponent}, 
+    
+    {path: 'Mostre/:id', component: MostreComponentComponent} ,
+    
+    {path: 'Home', component:IntroductionComponent},
+
+];
--- Esercizio_10/src/app/mostre/mostre-component.component.ts
+++ Esercizio_10/src/app/mostre/mostre-component.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ServizioMuseoService } from '../servizio-museo.service';
 
 @Component({
   selector: 'app-mostre-component',
@@ -10,4 +12,28 @@ import { Component } from '@angular/core';
 })
 export class MostreComponentComponent {
 
+  identificatore:any;
+  nome:any;
+  artisti:any;
+  descrizione:any;
+  datainizio:any;
+  datafine:any;
+  immagine:any;
+  costo:any;
+  citta:any;
+  constructor(private route: ActivatedRoute, public Servizio:ServizioMuseoService){
+
+  }
+  ngOnInit(){
+ 
+    this.identificatore = this.route.snapshot.paramMap.get('id');
+    this.nome=this.Servizio.dettagli[this.identificatore].nome
+    this.artisti=this.Servizio.dettagli[this.identificatore].artisti
+    this.descrizione=this.Servizio.dettagli[this.identificatore].descrizione
+    this.datainizio=this.Servizio.dettagli[this.identificatore].datainizio
+    this.datafine=this.Servizio.dettagli[this.identificatore].datafine
+    this.immagine=this.Servizio.dettagli[this.identificatore].immagine
+    this.costo=this.Servizio.dettagli[this.identificatore].costo
+    this.citta=this.Servizio.dettagli[this.identificatore].citta    
+  }
 }
